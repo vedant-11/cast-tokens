abb41cf36b976ff66532c60383dd1046ebbc6c20
Vedant Singh <69393820+vedant-11@users.noreply.github.com>
2022-02-10 18:51:12 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 
-const alchemyKey = "o7JzR-sAztJKdWH5Sk63wPmSVhklHzWg";
+const alchemyKey = "";
 
 const mumbaiPrivateKey =
-  "73d93254656026ea321c599c9912f7a56cf81f0a91a702b359783a2715ae9829";
+  "";
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -14,7 +14,7 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/o7JzR-sAztJKdWH5Sk63wPmSVhklHzWg",
+      url: "",
       accounts: [`0x${mumbaiPrivateKey}`],
     },
   },
